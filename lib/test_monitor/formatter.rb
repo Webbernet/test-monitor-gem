73feb72a63d528f6b9e41d95886c9bf19630e83b
Ivan Kuznetsov <me@jeiwan.ru>
2018-05-08 14:22:25 +0700

    Register the formatter

--- lib/test_monitor/formatter.rb
+++ lib/test_monitor/formatter.rb
@@ -2,6 +2,8 @@ require "rspec"
 
 module TestMonitor
   class Formatter < RSpec::Core::Formatters::ProgressFormatter
+    RSpec::Core::Formatters.register self, :dump_summary
+
     def dump_summary(summary)
       super(summary)
       puts("Hello from TestMonitor")
