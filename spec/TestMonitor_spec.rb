681224a544771b5cc7c054487a31274d827eb899
Ivan Kuznetsov <me@jeiwan.ru>
2018-05-08 15:14:55 +0700

    Set up tests

--- spec/TestMonitor_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe TestMonitor do
-  it "has a version number" do
-    expect(TestMonitor::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
---
+++ spec/lib/formatter_spec.rb
@@ -0,0 +1,7 @@
+describe TestMonitor::Formatter do
+  describe 'stuff' do
+    it 'does' do
+      expect(true).to be false
+    end
+  end
+end
